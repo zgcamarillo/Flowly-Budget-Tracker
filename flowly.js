10352d54599e214a21f12255f90b1309c5169aae
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2025-12-06 23:50:22 -0500

    adding date plus name entry input in js

--- flowly.js
+++ flowly.js
@@ -26,7 +26,7 @@ class Budget {
 
 //utility- helper functions
 function capitalize(text) {
-    return text.charAt(0).toUpperCase + text.slice(1);
+    return text.charAt(0).toUpperCase() + text.slice(1);
     //charAt(0) -grabbing character at said index.
 }
 
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const budget = new Budget();
 
     //POPUP variables
-    const welcomePopup = document.querySelector("welcome-popup");
+    const welcomePopup = document.querySelector("#welcome-popup");
     const nameInput = document.querySelector("#name-input");
     const nameSubmitButton = document.querySelector("#name-submit");
     const userNameSpan = document.querySelector("#user-name");
@@ -58,6 +58,34 @@ document.addEventListener("DOMContentLoaded", () => {
     const addExpenseBtn = document.querySelector("add-expense-btn");
     const addExpenseContaiiner = document.querySelector("add-expense-container");
     const submitExpenseBtn = document.querySelector("submit-expense-btn");
-    const expenseInputAmount = document
+    const expenseInputAmount = document.querySelector("#expense-input-amount");
+    const totalExpensesSpan = document.querySelector("#expenseTotal");
+
+    //budget total
+    const amountAvailableSpan = document.querySelector("#amount-available");
+
+    //show date 
+    const days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    todaysDateSpan.textContent = days[new Date().getDay()];
+        //date- built in object 
+        //new Date creates an object with the current date
+        //getDay an object that returns the number index of the day of the week 
+    
+    //taking users name and putting it in the main
+    nameSubmitButton.addEventListener("click", () => {
+        const nameEntry = nameInput.value.trim();
+        //trim()-remove spaces around 
+
+        if(!nameEntry) { //if NOT name entry
+            alert("Please enter your name!");
+            return;
+        }
+        //replace name into main- use capitalize function 
+        userNameSpan.textContent = capitalize(nameEntry);
+        welcomePopup.style.display = "none";
+    });
+
+
+
 
 })
